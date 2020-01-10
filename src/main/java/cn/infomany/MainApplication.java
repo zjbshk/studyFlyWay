82a26e50182eb8920bc2d433459b496a3f915022
占金彬 <oncwnuKss215Ng_Gw5KROX055GoQ@git.weixin.qq.com>
2020-01-10 13:49:58 +0800

    提交flyway使用dome

---
+++ src/main/java/cn/infomany/MainApplication.java
@@ -0,0 +1,17 @@
+package cn.infomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: springboot主程序
+ * @author: zhanjinbing
+ * @data: 2020-01-10 12:54
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ target/classes/db/migration/V2__test.sql
@@ -0,0 +1,4 @@
+use testdb;
+
+insert into person (first, last, dateofbirth, placeofbirth)
+values ('zjb', 'sdfds', STR_TO_DATE('05/10/1982', '%m/%d/%Y'), 'Erzeeincan');
